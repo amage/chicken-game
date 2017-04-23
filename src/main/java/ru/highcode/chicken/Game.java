5b20b319b773c55056bd8569be249ec901c4611f
Iurii Shchekochikhin <amagex@gmail.com>
2017-04-23 14:14:01 +0300

    some changes

--- src/main/java/ru/highcode/chicken/Game.java
+++ src/main/java/ru/highcode/chicken/Game.java
@@ -1,5 +1,7 @@
 package ru.highcode.chicken;
 
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -10,10 +12,13 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Game extends Application {
     public Game() {
@@ -26,11 +31,25 @@ public class Game extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Chicken Game");
+        primaryStage.setFullScreen(true);
 
+        final Scene loginScene = loginScene();
         final Image car1 = new Image("file:car.png");
         final Image car2 = new Image("file:car2.png");
         final ImageView imgView1 = new ImageView(car1);
         final ImageView imgView2 = new ImageView(car2);
+
+        final Path path = new Path();
+        path.getElements().add(new MoveTo(20, 20));
+        final PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(4000));
+        pathTransition.setPath(path);
+        pathTransition.setNode(imgView1);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setAutoReverse(true);
+        pathTransition.play();
+
         final VBox vBox = new VBox();
         vBox.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         final Text text = new Text(GameData.load().getTexts().get("1"));
@@ -44,7 +63,13 @@ public class Game extends Application {
 
         final Scene scene = new Scene(vBox);
         primaryStage.setScene(scene);
+        // primaryStage.setScene(loginScene);
         primaryStage.show();
+    }
+
+    private Scene loginScene() {
+        final VBox vBox = new VBox();
+        return new Scene(vBox);
     }
 
 }
