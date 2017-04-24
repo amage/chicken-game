63ab78a16751be5a163c2e3237ade5a96356d2c4
Iurii Shchekochikhin <amagex@gmail.com>
2017-04-24 11:21:21 +0300

    add key handler

--- src/main/java/ru/highcode/chicken/Game.java
+++ src/main/java/ru/highcode/chicken/Game.java
@@ -3,32 +3,23 @@ package ru.highcode.chicken;
 import java.io.IOException;
 
 import javafx.animation.AnimationTimer;
-import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class Game extends Application {
     private static GameData DATA;
@@ -36,7 +27,7 @@ public class Game extends Application {
     static {
         try {
             DATA = GameData.load();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
@@ -51,7 +42,7 @@ public class Game extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Chicken Game");
-//        primaryStage.setFullScreen(true);
+        // primaryStage.setFullScreen(true);
 
         final Scene scene = gameScene();
         primaryStage.setScene(scene);
@@ -59,9 +50,9 @@ public class Game extends Application {
     }
 
     private Scene textScene(String text) {
-        VBox pane = new VBox();
+        final VBox pane = new VBox();
         pane.setAlignment(Pos.CENTER);
-        Text t = new Text(text);
+        final Text t = new Text(text);
         t.setWrappingWidth(800);
         t.setTextAlignment(TextAlignment.JUSTIFY);
         pane.setSpacing(50);
@@ -71,26 +62,28 @@ public class Game extends Application {
     }
 
     private Scene loginScene() {
-        GridPane pane = new GridPane();
+        final GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
         pane.setHgap(20);
         pane.setVgap(20);
         pane.add(new Label("Данные об испытуемом"), 0, 0, 2, 1);
         pane.add(new Label("Номер"), 0, 1);
         pane.add(new TextField(), 1, 1);
-        HBox bbox = new HBox();
-        Button button = new Button("Старт");
+        final HBox bbox = new HBox();
+        final Button button = new Button("Старт");
         bbox.getChildren().add(button);
         bbox.setAlignment(Pos.BOTTOM_RIGHT);
         pane.add(bbox, 0, 2, 2, 1);
         return new Scene(pane);
     }
 
+    boolean run = false;
+
     private Scene gameScene() {
-        VBox pane = new VBox();
+        final VBox pane = new VBox();
         pane.setAlignment(Pos.CENTER);
 
-        GridPane scorePane = new GridPane();
+        final GridPane scorePane = new GridPane();
 
         scorePane.add(new Text("Total Bank Account: "), 0, 0);
         scorePane.add(new Text("0"), 1, 0);
@@ -100,23 +93,35 @@ public class Game extends Application {
         pane.getChildren().add(scorePane);
         pane.getChildren().add(new Text("Светофор"));
 
-        CarWay carWay = new CarWay();
+        final CarWay carWay = new CarWay();
         carWay.setBorder(new Border(new BorderStroke(Color.BLUE, BorderStrokeStyle.DASHED, null, null)));
 
         pane.getChildren().add(carWay);
-
-        long startNanoTime = System.nanoTime();
-        AnimationTimer at = new AnimationTimer() {
+        final long startNanoTime = System.nanoTime();
+        final AnimationTimer at = new AnimationTimer() {
             @Override
             public void handle(long currentNanoTime) {
-                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-                double v = 50;
-                double x = -200;
-                carWay.setCarPos(x + v * t);
+                final double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                final double v = 50;
+                final double x = -200;
+                if (run) {
+                    carWay.setCarPos(carWay.getCarPos() + 1);
+                }
             }
         };
         at.start();
 
+        carWay.setFocusTraversable(true);
+        carWay.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.SPACE) {
+                run = true;
+            }
+        });
+        carWay.setOnKeyReleased(e -> {
+            if (e.getCode() == KeyCode.SPACE) {
+                run = false;
+            }
+        });
         return new Scene(pane, 800, 600);
     }
 }
